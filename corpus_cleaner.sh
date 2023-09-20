6973d167ed12cbb114e7034cf38a5e571d377c50
lenastochastic <baraquin.lena@gmail.com>
2023-09-20 20:32:47 +0200

    modification corpus_cleaner.sh et ajout du repertoire corpora

--- corpus_cleaner.sh
+++ corpus_cleaner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Vérifie si le nombre d'arguments est égal à 1 (le nom du fichier)
+# Vérifie si le nombre d'arguments est égal à 1 (le nom du fichier d'entrée)
 if [ "$#" -ne 1 ]; then
     echo "Utilisation : $0 <nom_fichier>"
     exit 1
 fi
 
-# Vérifie si le fichier existe
+# Vérifie si le fichier d'entrée existe
 if [ ! -f "$1" ]; then
     echo "Le fichier $1 n'existe pas."
     exit 1
@@ -16,11 +16,10 @@ fi
 fichier_entree="$1"
 
 # Nom du fichier de sortie
-fichier_sortie="${fichier_entree%.txt}.txt"
+fichier_sortie="${fichier_entree%.txt}_nettoye.txt"
 
-# Utilise la commande 'sed' pour supprimer les caractères non alphanumériques
-sed "s/[^[:alnum:]' ]//g" "$fichier_entree" > "$fichier_sortie"
-#sed "s/\n/ /g" "$fichier_sortie" > "$fichier_sortie"
-#sed "s/ \+/ /g" "$fichier_sortie" > "$fichier_sortie"
+# Appliquer les opérations de nettoyage sur le fichier
+sed 's/[^[:alnum:] ]/ /g' "$fichier_entree" | tr '\n' ' ' | tr -s ' ' | tr '[:upper:]' '[:lower:]' > "$fichier_sortie"
 
 echo "Le texte a été nettoyé et enregistré dans $fichier_sortie."
+
