2e9044ce767c65a81fd338d0a3026a499eaf36c8
lenastochastic <baraquin.lena@gmail.com>
2023-09-22 15:57:50 +0200

    adaptation de la distance de stella

--- distance.py
+++ distance.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 import json
 import sys #arguments
 import matplotlib.pyplot as plt
@@ -57,17 +58,54 @@ def mixed_distance(k, match, medium_distance):
     match=(1-k)*match
     return medium_distance+match
 
+def norme(liste):
+    n = 0
+    p = 1
+    n = np.linalg.norm(liste, ord=p)
+    return n
+
+def farthest_point_distance(liste):
+    d = 0
+    r = 1 #les valeurs des probas des caractères suivants sont comprises entre 0 et 1, le rayon de la boule de l'espace des probabilités est donc de 1 (pas très rigoureuse cette explication)
+    p = 1 #on utilise la norme 1
+    d = norme(liste) + r
+    return d
+
+def distance_stella(D1,D2):
+    d = 0
+#    i = 0
+    
+    for m in D1.keys():
+        if m in D2.keys():
+            diff_list = []
+            for i in range(len(D1[m])):
+                diff_list.append(D1[m][i]-D2[m][i])
+            d += norme(diff_list)
+        else :
+            d += farthest_point_distance(D1[m])
+#        i += 1
+    
+    for m in D2.keys():
+        if m not in D1.keys():
+            d += farthest_point_distance(D2[m])
+#            i += 1
+    
+#    d = d/i
+    return d
+
 
 if __name__ == "__main__":
     file_dictionary_1 = sys.argv[1]
     file_dictionary_2 = sys.argv[2]
     dictionary_1=load_dictionary(file_dictionary_1)
     dictionary_2=load_dictionary(file_dictionary_2)
-    d = dictionary_distance(dictionary_1, dictionary_2)
-    d.sort()
+    d = distance_stella(dictionary_1, dictionary_2)
     print(d)
-    plt.plot(d)
-    plt.show()
+#    d = dictionary_distance(dictionary_1, dictionary_2)
+#    d.sort()
+#    print(d)
+#    plt.plot(d)
+#    plt.show()
 #    k=0.5
 #    m_d=mixed_distance(k, d[0], d[1])
 #    print(f"match, distance moyenne {d}")
